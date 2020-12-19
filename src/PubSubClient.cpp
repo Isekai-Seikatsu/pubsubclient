ccebdb62e3a34411cb17ca6547f008b9009b0989
isekai <brian1689878638@gmail.com>
2020-12-20 00:00:23 +0800

    Avoid strnlen func not found

--- src/PubSubClient.cpp
+++ src/PubSubClient.cpp
@@ -7,6 +7,17 @@
 
 #include "PubSubClient.h"
 #include "Arduino.h"
+#include <string.h>
+
+size_t strnlen(const char *s, size_t maxlen) {
+	size_t len;
+
+	for (len = 0; len < maxlen; len++, s++) {
+		if (!*s)
+			break;
+	}
+	return (len);
+}
 
 PubSubClient::PubSubClient() {
     this->_state = MQTT_DISCONNECTED;
